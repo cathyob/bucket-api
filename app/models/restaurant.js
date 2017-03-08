ebfa7ea50acfaed1a7b959dc2d8171a3f8bd95d7
“newayliu1” <newayliu1@gmail.com>
2017-03-08 11:27:47 -0500

    Create Restaurant Schema
    - Created Restaurant Schema with notes, achieved, location, type
    and _owner attributes, working with Dan and Cathy. We will change
    and add more validation in schema later.

---
+++ app/models/restaurant.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const restaurantSchema = new mongoose.Schema({
+  // TODO come back and determine if min/max length required for notes TODO //
+  notes: {
+    type: String,
+  },
+  achieved: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  location: {
+    type: String,
+    required:true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  _owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, {
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: function (doc, ret, options) {
+      let userId = (options.user && options.user._id) || false;
+      ret.editable = userId && userId.equals(doc._owner);
+      return ret;
+    },
+  },
+});
+
+restaurantSchema.virtual('length').get(function length() {
+  return this.text.length;
+});
+
+const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+module.exports = Restaurant;
